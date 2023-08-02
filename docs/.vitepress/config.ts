9d8b1a8d86b50dd80a8992ed293a7c6131e167e2
NishaSharma14 <sharmanis14@gmail.com>
2023-08-02 13:17:28 +0200

    docs:update theme

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,56 +1,108 @@
-import { defineConfig } from 'vitepress'
+import { defineConfigWithTheme } from "vitepress";
+import type { ThemeConfig } from '@hempworks/pilgrim'
+import config from '@hempworks/pilgrim/config'
 
-// https://vitepress.dev/reference/site-config
-export default defineConfig({
-  title: "NMR Predict",
-  description: "Microservice designed to predict the NMR spectrum of a molecule quickly and accurately.",
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Docs', link: '/introduction' },
-      { text: 'API', link: '/introduction' },
+export default defineConfigWithTheme<ThemeConfig>({
+    extends: config,
+    title: 'Envoyer',
+    description: 'Zero Downtime PHP Deployments',
+    base: '/',
+    cleanUrls: false,
+    srcDir: 'src',
+
+    head: [
+        ['link', {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: 'https://envoyer.io/apple-touch-icon.png',
+        }],
+        ['link', {
+            rel: 'icon',
+            sizes: '16x16',
+            type: 'image/png',
+            href: 'https://envoyer.io/favicon-16x16.png',
+        }],
+        ['link', {
+            rel: 'icon',
+            sizes: '32x32',
+            type: 'image/png',
+            href: 'https://envoyer.io/favicon-32x32.png',
+        }],
+        ['link', {
+            rel: 'mask-icon',
+            href: 'https://envoyer.io/safari-pinned-tab.svg',
+        }],
+        ['meta', {
+            name: 'msapplication-TileColor',
+            content: '#18b69b',
+        }],
+        ['meta', {
+            name: 'msapplication-TileImage',
+            content: 'envoyer.io/mstile-144x144.png',
+        }],
+        ['meta', {
+            property: 'og:image',
+            content: 'https://envoyer.io/social-share.png',
+        }],
+        ['meta', {
+            property: 'twitter:card',
+            content: 'summary_large_image',
+        }],
+        ['meta', {
+            property: 'twitter:image',
+            content: 'https://envoyer.io/social-share.png',
+        }],
     ],
 
-    sidebar: [
-      {
-        text: 'Welcome',
-        items: [
-          { text: 'Introduction', link: '/introduction' },
-          { text: 'Versions', link: '/versions' },
-          { text: 'Architecture', link: '/architecture' },
-        ]
-      },
-      {
-        text: 'Usage',
-        items: [
-          //{ text: 'Public API', link: '/public-api' },
-          { text: 'Docker', link: '/docker' },
-          { text: 'Cluster Deployment (K8S)', link: '/cluster-deployment' }
-        ]
-      },
-      {
-        text: 'Modules',
-        items: [
-          { text: 'Lightweight-Registration', link: '/lightweight-registration' },
-          { text: 'Spectra', link: '/spectra' },
-          { text: 'Training', link: '/training' },
-          { text: 'Validation', link: '/validation' },
-        ]
-      },
-      {
-        text: 'Development',
-        items: [
-          { text: 'Local Installation', link: '/installation' },
-          { text: 'License', link: '/license' },
-          { text: 'Issues/Feature requests', link: '/issues' },
-          { text: 'Contributors', link: '/contributors' }
-        ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
+    themeConfig: {
+        logo: {
+            light: '/logo.svg',
+            dark: '/logo-dark.svg',
+        },
+        nav: [
+            { text: 'Envoyer', link: 'https://envoyer.io' },
+            { text: 'Video Tutorials', link: 'https://laracasts.com/series/envoyer' },
+        ],
+        sidebar: [
+            {
+                text: "Getting Started",
+                items: [
+                    { text: 'Introduction', link: '/introduction' },
+                ],
+            },
+            {
+                text: "Accounts",
+                items: [
+                    { text: 'Your Account', link: '/accounts/your-account' },
+                    { text: 'Source Control', link: '/accounts/source-control' },
+                ],
+            },
+            {
+                text: "Projects",
+                items: [
+                    { text: 'Management', link: '/projects/management' },
+                    { text: 'Servers', link: '/projects/servers' },
+                    { text: 'Deployment Hooks', link: '/projects/deployment-hooks' },
+                    { text: 'Heartbeats', link: '/projects/heartbeats' },
+                    { text: 'Notifications', link: '/projects/notifications' },
+                    { text: 'Collaborators', link: '/projects/collaborators' },
+                ],
+            },
+        ],
+        search: {
+            provider: 'local',
+            options: {
+                placeholder: 'Search Envoyer Docs...',
+            },
+        }
+    },
+    vite: {
+        server: {
+            host: true,
+            fs: {
+                // for when developing with locally linked theme
+                allow: ['../..']
+            }
+        },
+    }
 })
